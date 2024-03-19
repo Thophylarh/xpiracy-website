afe828a14c0a01288158fa5db6032e46f330757c
micheal <micheal@budpay.com>
2024-03-19 20:38:07 +0100

    updated

--- src/pages/landingpage/Hero.jsx
+++ src/pages/landingpage/Hero.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import PifModal from "../../components/modals/PifModal";
-import BookModal from "../../components/modals/BookModal";
-import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import React, { useState } from 'react';
+import PifModal from '../../components/modals/PifModal';
+import BookModal from '../../components/modals/BookModal';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 
 const Hero = () => {
   const [pifModalOpen, setPifModalOpen] = useState(false);
@@ -25,9 +25,9 @@ const Hero = () => {
           <h4 className="text-[14px] md:text-[16px]">WHAT WOULD JESUS DO?</h4>
         </div>
 
-        <h1 className="md:text-[4rem] text-[2rem] font-bold">CHRISTPIRACY</h1>
+        <h1 className="md:text-[4rem] text-[2rem] font-bold">CHRISTSPIRACY</h1>
         <h4 className="md:text-[16px] text-[14px] font-semibold">
-          IN THEATRES <span className="text-[#e93c24]">MARCH 20TH, 24TH.</span>{" "}
+          IN THEATRES <span className="text-[#e93c24]">MARCH 20TH, 24TH.</span>{' '}
         </h4>
         {/* <button className="p-2 my-4 underline rounded text-[13px]">Sign Up For More</button>
         <p className="text-white">Receive extra content, community, resources, and more. </p> */}
@@ -35,13 +35,13 @@ const Hero = () => {
           onClick={handlePifModalOpen}
           className="bg-[#E93C24] p-4 rounded-[40px] my-4 text-sm flex items-center hover:bg-[#f86d5a]"
         >
-          <ConfirmationNumberIcon sx={{ color: "#fff", fontSize: "18px" }} />
+          <ConfirmationNumberIcon sx={{ color: '#fff', fontSize: '18px' }} />
           <p className="ml-2">Pay it Forward</p>
         </button>
 
         <p className="text-[12px] text-[#d8d6d6] md:w-[400px]">
           Allow someone else to watch Christspiracy for free. Your generosity
-          helps spread this message far and wide.{" "}
+          helps spread this message far and wide.{' '}
         </p>
       </div>
       <PifModal
